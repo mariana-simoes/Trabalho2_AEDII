a7ab285eb7bfd901d58715cb48cde46a46502431
AndreMalmsteen <167015907+AndreMalmsteen@users.noreply.github.com>
2026-05-06 19:56:38 -0400

    Atualização do header do TAD_arv
    Correção de bugs e funções completas necessárias para a construção da questão 2

--- TAD_arv.h
+++ TAD_arv.h
@@ -1,23 +1,33 @@
 #ifndef ARVORE_H
 #define ARVORE_H
 
-typedef struct{ 
-    char dado; 
-    Arv* esq;
-    Arv* dir;
-} Arv; 
+#include <stdio.h>
+
+#define TAM 64
+
+typedef struct arv {
+    int id; // identifica o pacote
+    char dado[TAM];
+    struct arv* esq;
+    struct arv* dir;
+} arv; 
 
 /// ARVORE BINARIA ///
 
-Arv* inicializa(Arv*raiz);
-Arv* insereArv(int tam);
-void podaArv();
-char buscaArv();
+arv* inicializa(arv*raiz);
+arv* insereArv(arv *raiz, int id, const char *dado); // onde, id, ponteiro para o dado
+void podaArv(arv *raiz); // aqui, toda a árvore é liberada
+char buscaArv(arv *raiz, int id); // onde e qual dado quer buscar
 
 
-void preFix(Arv*raiz);
-void Fix(Arv*raiz);
-void posFix(Arv*raiz);
+/* Percursos da árvore binária. Única diferença entre elas é só quando
+a raiz é visitada em relação aos filhos*/
+
+void preFix(arv*raiz); // pré ordem (raiz -> esquerda -> direita). Visita o nó antes dos filhos
+void inFix(arv*raiz); // em ordem (esquerda -> raiz -> direita). Visita o nó entre os filhos. Produz os elementos na ord. crescente
+void posFix(arv*raiz); // pós ordem (esquerda -> direita -> raiz). Visita o nó depois dos filhos. Útil para liberar a árvore.
+
+void montarArquivo (arv *raiz, FILE *arq);
 
 
 #endif
